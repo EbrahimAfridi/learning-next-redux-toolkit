92f786ae6412dbc1f38aa509855a3bbfe22881db
Ebrahim Afridi <afridiebrahimck@gmail.com>
2024-05-19 15:20:16 +0530

    Added: shadcn/ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
     return (
       <>
-        <h1>Home</h1>
         <Image
           src="/vercel.svg"
           alt="Vercel Logo"
           width={72}
           height={16}
         />
+        <Button variant="link">Button</Button>
       </>
     );
-}
+  }
